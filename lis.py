09336f811f8212f6ba0135661b3649ecd74c3a97
Subham Banerjee <32572976+subban358@users.noreply.github.com>
2019-06-09 09:04:52 +0530

    Create lis.py

---
+++ lis.py
@@ -0,0 +1,16 @@
+'''
+finding the longest increasing subsequence
+like in this example [3 10 40 80]
+so we have to write a function which returns the length of max length of longest increasing subsequence which is=4 here !! 
+'''
+
+def lis(a,b):
+	for i in range(1,len(a)):
+		for j in range(0,i):
+			if(a[i]>=a[j] and b[i]<b[j]+1):
+				b[i]=b[j]+1
+	return max(b)			
+
+a=[50, 3, 10, 7, 40, 80] #subsequence=[3 10 40 80]=>> length=4
+b=[1]*len(a) #making a list initializing all values by 1
+print("The length of longest increasing subsequence is:"+str(lis(a,b)))	
