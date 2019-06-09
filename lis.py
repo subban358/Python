bfb64a28a0f178a4a5138c9e300d143aaf4fcfdd
Subham Banerjee <32572976+subban358@users.noreply.github.com>
2019-06-09 09:10:43 +0530

    Update lis.py

--- lis.py
+++ lis.py
@@ -7,7 +7,7 @@ so we have to write a function which returns the length of max length of longest
 def lis(a,b):
 	for i in range(1,len(a)):
 		for j in range(0,i):
-			if(a[i]>=a[j] and b[i]<b[j]+1):
+			if(a[i]>=a[j] and b[i]<=b[j]+1):
 				b[i]=b[j]+1
 	return max(b)			
 
