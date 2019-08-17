9e3813c391fa6bb20a1c87bb8dd49f352ba097fe
Subham Banerjee <32572976+subban358@users.noreply.github.com>
2019-08-17 20:48:33 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -120,7 +120,7 @@ def sudokuSolver(board):
         # go through all the possibilities and call the the function
         # again and again
         for x in range(1, 10):
-            if not possiblities[x] == 0:
+            if possiblities[x] != 0:
                 board[i][j] = possiblities[x]
                 sudokuSolver(board)
         # backtrack
@@ -128,91 +128,18 @@ def sudokuSolver(board):
 
 
 def main():
-    SudokuBoard = [[0 for x in range(9)] for x in range(9)]
-    SudokuBoard[0][0] = 0
-    SudokuBoard[0][1] = 0
-    SudokuBoard[0][2] = 0
-    SudokuBoard[0][3] = 3
-    SudokuBoard[0][4] = 0
-    SudokuBoard[0][5] = 0
-    SudokuBoard[0][6] = 2
-    SudokuBoard[0][7] = 0
-    SudokuBoard[0][8] = 0
-    SudokuBoard[1][0] = 0
-    SudokuBoard[1][1] = 0
-    SudokuBoard[1][2] = 0
-    SudokuBoard[1][3] = 0
-    SudokuBoard[1][4] = 0
-    SudokuBoard[1][5] = 8
-    SudokuBoard[1][6] = 0
-    SudokuBoard[1][7] = 0
-    SudokuBoard[1][8] = 0
-    SudokuBoard[2][0] = 0
-    SudokuBoard[2][1] = 7
-    SudokuBoard[2][2] = 8
-    SudokuBoard[2][3] = 0
-    SudokuBoard[2][4] = 6
-    SudokuBoard[2][5] = 0
-    SudokuBoard[2][6] = 3
-    SudokuBoard[2][7] = 4
-    SudokuBoard[2][8] = 0
-    SudokuBoard[3][0] = 0
-    SudokuBoard[3][1] = 4
-    SudokuBoard[3][2] = 2
-    SudokuBoard[3][3] = 5
-    SudokuBoard[3][4] = 1
-    SudokuBoard[3][5] = 0
-    SudokuBoard[3][6] = 0
-    SudokuBoard[3][7] = 0
-    SudokuBoard[3][8] = 0
-    SudokuBoard[4][0] = 1
-    SudokuBoard[4][1] = 0
-    SudokuBoard[4][2] = 6
-    SudokuBoard[4][3] = 0
-    SudokuBoard[4][4] = 0
-    SudokuBoard[4][5] = 0
-    SudokuBoard[4][6] = 4
-    SudokuBoard[4][7] = 0
-    SudokuBoard[4][8] = 9
-    SudokuBoard[5][0] = 0
-    SudokuBoard[5][1] = 0
-    SudokuBoard[5][2] = 0
-    SudokuBoard[5][3] = 0
-    SudokuBoard[5][4] = 8
-    SudokuBoard[5][5] = 6
-    SudokuBoard[5][6] = 1
-    SudokuBoard[5][7] = 5
-    SudokuBoard[5][8] = 0
-    SudokuBoard[6][0] = 0
-    SudokuBoard[6][1] = 3
-    SudokuBoard[6][2] = 5
-    SudokuBoard[6][3] = 0
-    SudokuBoard[6][4] = 9
-    SudokuBoard[6][5] = 0
-    SudokuBoard[6][6] = 7
-    SudokuBoard[6][7] = 6
-    SudokuBoard[6][8] = 0
-    SudokuBoard[7][0] = 0
-    SudokuBoard[7][1] = 0
-    SudokuBoard[7][2] = 0
-    SudokuBoard[7][3] = 7
-    SudokuBoard[7][4] = 0
-    SudokuBoard[7][5] = 0
-    SudokuBoard[7][6] = 0
-    SudokuBoard[7][7] = 0
-    SudokuBoard[7][8] = 0
-    SudokuBoard[8][0] = 0
-    SudokuBoard[8][1] = 0
-    SudokuBoard[8][2] = 9
-    SudokuBoard[8][3] = 0
-    SudokuBoard[8][4] = 0
-    SudokuBoard[8][5] = 5
-    SudokuBoard[8][6] = 0
-    SudokuBoard[8][7] = 0
-    SudokuBoard[8][8] = 0
+    SudokuBoard = [[0, 0, 0, 3, 0, 0, 2, 0, 0],
+                   [0, 0, 0, 0, 0, 8, 0, 0, 0],
+                   [0, 7, 8, 0, 6, 0, 3, 4, 0],
+                   [0, 4, 2, 5, 1, 0, 0, 0, 0],
+                   [1, 0, 6, 0, 0, 0, 4, 0, 9],
+                   [0, 0, 0, 0, 8, 6, 1, 5, 0],
+                   [0, 3, 5, 0, 9, 0, 7, 6, 0],
+                   [0, 0, 0, 7, 0, 0, 0, 0, 0],
+                   [0, 0, 9, 0, 0, 5, 0, 0, 0]
+                   ]
     printBoard(SudokuBoard)
     sudokuSolver(SudokuBoard)
-
 
 
 if __name__ == "__main__":
